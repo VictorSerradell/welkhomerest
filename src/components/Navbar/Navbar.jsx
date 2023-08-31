1696cef9a1924b7a9be0eae6f854a9a7cb0e4970
VictorSerradell <victorserradellpinto@hotmail.com>
2023-08-31 18:29:41 +0200

    Add Elements
    Design basic

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Bars, Nav, NavIcon, NavLink } from './NavbarElements';
+
+function NavBar({ toggle }) {
+	return (
+		<>
+			<Nav>
+				<NavLink to='/'>Pizza</NavLink>
+				<NavIcon onClick={toggle}>
+					<p>Menu</p>
+					<Bars />
+				</NavIcon>
+			</Nav>
+		</>
+	);
+}
+
+export default NavBar;
